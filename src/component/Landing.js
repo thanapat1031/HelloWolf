e68610530cc3fc4f44fb3bf9fb60cf5222232cd6
thanapat1031 <thanapat1031@gmail.com>
2018-11-08 00:51:17 +0700

    [ADD] Landing

---
+++ src/component/Landing.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+// import Image from './bg-landing.png'
+
+
+
+const Bg = styled.div`
+background-image : url("/img/landing.png") ;
+
+`
+
+const Landing = () => (
+    <Bg>
+        <p>Hello Wolf</p>
+    </Bg>
+)
+
+
+
+
+
+
+export default Landing
