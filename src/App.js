687db1d1e381ab4d46f35ca7039312c4816fa544
Bew <thanapat1031@gmail.com>
2018-11-11 15:11:56 +0700

    [REMOVE]App-box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import Landing from './component/Landing'
-<<<<<<< HEAD
 import TimeLine from './component/TimeLine'
-import Box from './component/Box'
-=======
 import About from './component/About';
->>>>>>> d381c1327f6e944cb060d9da2be901fa22f4b17f
 
 
 class App extends Component {
@@ -13,14 +9,8 @@ class App extends Component {
     return (
       <div>
         <Landing />
-<<<<<<< HEAD
-        <TimeLine/>
-        <Box/>
-=======
-
         <About/>
-
->>>>>>> d381c1327f6e944cb060d9da2be901fa22f4b17f
+        <TimeLine/>
       </div>
     )
 }
